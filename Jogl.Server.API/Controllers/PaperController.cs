568e1ef5744418c9264b23386bd85188dadd483f
Filip <Filip>
2024-12-06 09:25:32 +0000

    removing association methods from paper controller

--- Jogl.Server.API/Controllers/PaperController.cs
+++ Jogl.Server.API/Controllers/PaperController.cs
@@ -136,53 +136,5 @@ namespace Jogl.Server.API.Controllers
             await _paperService.DeleteAsync(paper);
             return Ok();
         }
-
-        [Obsolete]
-        [HttpPost]
-        [Route("{id}/associate/{entityId}")]
-        [SwaggerOperation($"Associates a paper to the specified community entity")]
-        [SwaggerResponse((int)HttpStatusCode.NotFound, "No community entity or no paper was found for the specified id")]
-        [SwaggerResponse((int)HttpStatusCode.Conflict, "The paper is already added to the community entity's library")]
-        [SwaggerResponse((int)HttpStatusCode.Forbidden, "The currently logged user does not have the permissions to manage the community entity's library")]
-        [SwaggerResponse((int)HttpStatusCode.OK, "The paper has been associated with the entity")]
-        public async Task<IActionResult> AssociatePaper([FromRoute] string id, [FromRoute] string entityId)
-        {
-            var entity = _communityEntityService.GetEnriched(entityId, CurrentUserId);
-            if (entity == null)
-                return NotFound();
-
-            var paper = _paperService.Get(id, CurrentUserId);
-            if (paper == null)
-                return NotFound();
-
-            if (!entity.Permissions.Contains(Data.Enum.Permission.ManageLibrary))
-                return Forbid();
-
-            if (paper.FeedIds.Any(eid => string.Equals(eid, entityId, StringComparison.InvariantCultureIgnoreCase)))
-                return Conflict();
-
-            await _paperService.AssociateAsync(entityId, id);
-            return Ok();
-        }
-
-        [Obsolete]
-        [HttpDelete]
-        [Route("{id}/papers/{entityId}")]
-        [SwaggerOperation($"Disassociates the specified paper from the specified community entity")]
-        [SwaggerResponse((int)HttpStatusCode.NotFound, "No paper was found for the paper id or the paper isn't associated to the given community entity")]
-        [SwaggerResponse((int)HttpStatusCode.Forbidden, "The current user does not have the rights to manage papers for the community entity")]
-        [SwaggerResponse((int)HttpStatusCode.OK, $"The paper was disassociated from the entity")]
-        public async Task<IActionResult> DisassociatePaper([FromRoute] string id, [FromRoute] string entityId)
-        {
-            var entity = _communityEntityService.GetEnriched(entityId, CurrentUserId);
-            if (entity == null)
-                return NotFound();
-
-            if (!entity.Permissions.Contains(Permission.ManageLibrary))
-                return Forbid();
-
-            await _paperService.DisassociateAsync(entityId, id);
-            return Ok();
-        }
     }
 }
