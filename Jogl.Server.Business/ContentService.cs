f30bf6b91718113a9d314761b406c6860c325ebc
Filip <Filip>
2025-01-27 21:35:07 +0000

    #2286

--- Jogl.Server.Business/ContentService.cs
+++ Jogl.Server.Business/ContentService.cs
@@ -359,7 +359,7 @@ namespace Jogl.Server.Business
                 contentEntity.UserSource = ContentEntitySource.Post;
             }
 
-            return contentEntities.DistinctBy(ce => ce.Id).ToList();
+            return contentEntities.DistinctBy(ce => ce.FeedId).ToList();
         }
 
         public List<ContentEntity> ListThreadsForNode(string currentUserId, string nodeId, int page, int pageSize)
