6d12dded838df51d68cfac66a1ed5927ee86cdbd
Filip <Filip>
2025-02-24 20:13:08 +0000

    endpoint stub

---
+++ Jogl.Server.API/Model/InvitationMassUpsertModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Jogl.Server.API.Model
+{
+    public class InvitationMassUpsertModel
+    {
+        [JsonPropertyName("invitees")]
+        public required List<InvitationUpsertModel> Invitees { get; set; }
+
+        [JsonPropertyName("community_entity_ids")]
+        public required List<string> CommunityEntityIds { get; set; }
+    }
+}
