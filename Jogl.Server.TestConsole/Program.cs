e23b92e47878551e02b48c8c0ab8826d6817abaf
Filip <Filip>
2025-02-25 20:35:37 +0000

    migration

--- Jogl.Server.TestConsole/Program.cs
+++ Jogl.Server.TestConsole/Program.cs
@@ -23,59 +23,109 @@ var workspaceRepository = new WorkspaceRepository(config);
 
 var feeds = feedRepository.List(f => true);
 var workspaces = workspaceRepository.List(u => true);
+var contentEntities = contentEntityRepository.List(u => true);
+var comments = commentRepository.List(u => true);
 
-foreach (var feed in feeds)
-{
-    if (feed.Type != Jogl.Server.Data.FeedType.Workspace)
-        continue;
+//foreach (var feed in feeds)
+//{
+//    if (feed.Type != Jogl.Server.Data.FeedType.Workspace)
+//        continue;
 
-    var workspace = workspaces.FirstOrDefault(f => f.Id == feed.Id);
-    if (workspace == null)
-    {
-        await feedRepository.DeleteAsync(feed);
-        continue;
-    }
+//    var workspace = workspaces.FirstOrDefault(w => w.Id == feed.Id);
+//    if (workspace == null)
+//    {
+//        await feedRepository.DeleteAsync(feed);
+//        continue;
+//    }
 
-    if (workspace.Deleted)
-    {
-        await feedRepository.DeleteAsync(feed);
-        continue;
-    }
-}
+//    if (workspace.Deleted)
+//    {
+//        await feedRepository.DeleteAsync(feed);
+//        continue;
+//    }
+//}
 
-feeds = feedRepository.List(f => true);
+//feeds = feedRepository.List(f => true);
 
-foreach (var ufr in userFeedRecordRepository.List(u => !u.Deleted))
-{
-    var feed = feeds.FirstOrDefault(f => f.Id.ToString() == ufr.FeedId);
-    if (feed == null)
-    {
-        await userFeedRecordRepository.DeleteAsync(ufr);
-        continue;
-    }
+//foreach (var ufr in userFeedRecordRepository.List(u => !u.Deleted))
+//{
+//    var feed = feeds.FirstOrDefault(f => f.Id.ToString() == ufr.FeedId);
+//    if (feed == null)
+//    {
+//        await userFeedRecordRepository.DeleteAsync(ufr);
+//        continue;
+//    }
 
-    if (feed.Deleted)
-    {
-        await userFeedRecordRepository.DeleteAsync(ufr);
-        continue;
-    }
-}
+//    if (feed.Deleted)
+//    {
+//        await userFeedRecordRepository.DeleteAsync(ufr);
+//        continue;
+//    }
+//}
 
-foreach (var ucer in userContentEntityRecordRepository.List(u => !u.Deleted))
-{
-    var feed = feeds.FirstOrDefault(f => f.Id.ToString() == ucer.FeedId);
-    if (feed == null)
-    {
-        await userContentEntityRecordRepository.DeleteAsync(ucer);
-        continue;
-    }
+//foreach (var ucer in userContentEntityRecordRepository.List(u => !u.Deleted))
+//{
+//    var feed = feeds.FirstOrDefault(f => f.Id.ToString() == ucer.FeedId);
+//    if (feed == null)
+//    {
+//        await userContentEntityRecordRepository.DeleteAsync(ucer);
+//        continue;
+//    }
 
-    if (feed.Deleted)
-    {
-        await userContentEntityRecordRepository.DeleteAsync(ucer);
-        continue;
-    }
-}
+//    if (feed.Deleted)
+//    {
+//        await userContentEntityRecordRepository.DeleteAsync(ucer);
+//        continue;
+//    }
+//}
+
+//foreach (var m in mentionRepository.List(m => true))
+//{
+//    var feed = feeds.FirstOrDefault(f => f.Id.ToString() == m.OriginFeedId);
+//    if (feed == null)
+//    {
+//        await mentionRepository.DeleteAsync(m);
+//        continue;
+//    }
+
+//    if (feed.Deleted)
+//    {
+//        await mentionRepository.DeleteAsync(m);
+//        continue;
+//    }
+
+//    if (m.OriginType == Jogl.Server.Data.MentionOrigin.ContentEntity)
+//    {
+//        var contentEntity = contentEntities.SingleOrDefault(ce => ce.Id.ToString() == m.OriginId);
+//        if (contentEntity == null)
+//        {
+
+
+//        }
+
+//        if (m.OriginFeedId != contentEntity.FeedId)
+//        {
+//            m.OriginFeedId = contentEntity.FeedId;
+//            await mentionRepository.UpdateAsync(m);
+//        }
+//    }
+
+//    if (m.OriginType == Jogl.Server.Data.MentionOrigin.Comment)
+//    {
+//        var comment = comments.SingleOrDefault(c => c.Id.ToString() == m.OriginId);
+//        if (comment == null)
+//        {
+
+
+//        }
+
+//        if (m.OriginFeedId != comment.FeedId)
+//        {
+//            m.OriginFeedId = comment.FeedId;
+//            await mentionRepository.UpdateAsync(m);
+//        }
+//    }
+//}
 
 Console.WriteLine("Done");
 Console.ReadLine();
