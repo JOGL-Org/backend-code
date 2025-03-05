8fa34d2df1bc9fe66f827020d4d661b837100df1
Filip <Filip>
2025-03-05 22:06:04 +0000

    lix caching

--- Jogl.Server.Lix/LixFacade.cs
+++ Jogl.Server.Lix/LixFacade.cs
@@ -2,28 +2,35 @@
 using RestSharp;
 using Microsoft.Extensions.Logging;
 using Jogl.Server.Lix.DTO;
-using System.Text.Json;
+using Microsoft.Extensions.Caching.Memory;
 
 namespace Jogl.Server.Lix
 {
     public class LixFacade : ILixFacade
     {
         private readonly IConfiguration _configuration;
-
-        public LixFacade(IConfiguration configuration)
-        {
-            _configuration = configuration;
-        }
-
+        private readonly IMemoryCache _memoryCache;
         private readonly ILogger<LixFacade> _logger;
 
-        public LixFacade(IConfiguration configuration, ILogger<LixFacade> logger)
+        public LixFacade(IConfiguration configuration, IMemoryCache memoryCache, ILogger<LixFacade> logger)
         {
             _configuration = configuration;
+            _memoryCache = memoryCache;
             _logger = logger;
         }
 
         public async Task<Profile> GetProfileAsync(string linkedInUrl)
+        {
+            if (!_memoryCache.TryGetValue<Profile>(linkedInUrl, out var profileData))
+            {
+                profileData = await GetProfileFromLixAsync(linkedInUrl);
+                _memoryCache.Set(linkedInUrl, profileData);
+            }
+
+            return profileData;
+        }
+
+        private async Task<Profile> GetProfileFromLixAsync(string linkedInUrl)
         {
             try
             {
@@ -32,9 +39,7 @@ namespace Jogl.Server.Lix
                 request.AddParameter("profile_link", linkedInUrl, ParameterType.QueryString, true);
                 request.AddHeader("Authorization", _configuration["Lix:ApiKey"]);
 
-                //var json = File.ReadAllText("bin/Debug/net8.0/data.txt");
-                //return JsonSerializer.Deserialize<Profile>(json);
-                var response =await client.ExecuteGetAsync<Profile>(request);
+                var response = await client.ExecuteGetAsync<Profile>(request);
                 return response.Data;
             }
             catch (Exception ex)
