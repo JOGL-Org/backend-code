d8a9535bf66a73eb4af3aa57c8dab6636ea0b0b4
filip@jogl.io <filip@jogl.io>
2025-04-22 22:46:01 +0100

    removing old code

--- Jogl.Server.SlackAgentAPI/Controllers/AdminController.cs
+++ Jogl.Server.SlackAgentAPI/Controllers/AdminController.cs
@@ -5,6 +5,8 @@ using Jogl.Server.Business;
 using Jogl.Server.URL;
 using Jogl.Server.Data;
 using Jogl.Server.Slack;
+using Jogl.Server.Conversation.Data;
+using Jogl.Server.Notifications;
 
 namespace Jogl.Server.API.Controllers
 {
@@ -12,21 +14,15 @@ namespace Jogl.Server.API.Controllers
     public class AdminController : ControllerBase
     {
         private readonly ISlackService _slackService;
-        private readonly IUserService _userService;
-        private readonly IUrlService _urlService;
-        private readonly IMembershipService _membershipService;
+        private readonly INotificationFacade _notificationFacade;
         private readonly IInterfaceChannelRepository _interfaceChannelRepository;
-        private readonly IInterfaceUserRepository _interfaceUserRepository;
         private readonly ILogger<MessageHandler> _logger;
 
-        public AdminController(ISlackService slackService, IUserService userService, IUrlService urlService, IMembershipService membershipService, IInterfaceChannelRepository interfaceChannelRepository, IInterfaceUserRepository interfaceUserRepository, ILogger<MessageHandler> logger)
+        public AdminController(ISlackService slackService, INotificationFacade notificationFacade, IInterfaceChannelRepository interfaceChannelRepository, IInterfaceUserRepository interfaceUserRepository, ILogger<MessageHandler> logger)
         {
             _slackService = slackService;
-            _userService = userService;
-            _urlService = urlService;
-            _membershipService = membershipService;
+            _notificationFacade = notificationFacade;
             _interfaceChannelRepository = interfaceChannelRepository;
-            _interfaceUserRepository = interfaceUserRepository;
             _logger = logger;
         }
 
@@ -56,57 +52,17 @@ namespace Jogl.Server.API.Controllers
                 if (user.Profile.Email != "thomas+99@JOGL.io")
                     continue;
 
-                var code = await _userService.GetOnetimeLoginCodeAsync(user.Profile.Email);
-                var url = _urlService.GetOneTimeLoginLink(user.Profile.Email, code);
-                var channelId = await _slackService.GetUserChannelIdAsync(channel.Key, user.Id);
-
-                var existingUser = _userService.GetForEmail(user.Profile.Email);
-                if (existingUser != null)
+                await _notificationFacade.NotifyAsync(Const.USER_JOINED_INTERFACE_CHANNEL, new UserJoined
                 {
-                    var existingInterfaceUser = _interfaceUserRepository.Get(iu => iu.UserId == existingUser.Id.ToString());
-                    if (existingInterfaceUser == null)
-                        await _interfaceUserRepository.CreateAsync(new InterfaceUser
-                        {
-                            CreatedByUserId = existingUser.Id.ToString(),
-                            CreatedUTC = DateTime.UtcNow,
-                            UserId = existingUser.Id.ToString(),
-                            ChannelId = channel.Id.ToString(),
-                            ExternalId = user.Id,
-                        });
-
-                    await _slackService.SendMessageAsync(channel.Key, channelId, $"Hello {user.Profile.FirstName}, it seems you already have a JOGL profile! You can log in <{url}|here>");
-                }
-                else
-                {
-                    var userId = await _userService.CreateAsync(new User
+                    ChannelExternalId = user.TeamId,
+                    User = new Conversation.Data.User
                     {
+                        Email = user.Profile.Email,
                         FirstName = user.Profile.FirstName,
                         LastName = user.Profile.LastName,
-                        Email = user.Profile.Email,
-                        Status = UserStatus.Verified,
-                        CreatedUTC = DateTime.UtcNow,
-                    });
-
-                    await _interfaceUserRepository.CreateAsync(new InterfaceUser
-                    {
-                        CreatedByUserId = userId,
-                        CreatedUTC = DateTime.UtcNow,
-                        UserId = userId,
-                        ChannelId = channel.Id.ToString(),
                         ExternalId = user.Id,
-                    });
-
-                    await _membershipService.AddMembersAsync([new Membership {
-                        CreatedByUserId = userId,
-                        CreatedUTC = DateTime.UtcNow,
-                        CommunityEntityId = channel.NodeId,
-                        CommunityEntityType = CommunityEntityType.Node,
-                        AccessLevel = AccessLevel.Member,
-                        UserId = userId,
-                    }]);
-
-                    await _slackService.SendMessageAsync(channel.Key, channelId, $"Hello {user.Profile.FirstName}, welcome to JOGL! We've set up your JOGL account on {user.Profile.Email}. You can set up your profile <{url}|here>");
-                }
+                    }
+                });
             }
 
             return Ok();
